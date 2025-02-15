1e9095c9d785534a34a3d2b17c6e55a65bced69c
Nathaniel Gorham <nathanielgorham@Nathaniels-MacBook-Air.local>
2025-02-15 08:50:06 -0500

    right to next line

--- kilo.c
+++ kilo.c
@@ -297,7 +297,10 @@ void editorMoveCursor(int key) {
         case ARROW_RIGHT:
             if (row && E.cx < row->size) {
                 E.cx++;
-            }            
+            } else if (row && E.cx == row->size) {
+                E.cy++;
+                E.cx = 0;
+            }  
             break;
         case ARROW_UP:
             if (E.cy != 0) {
